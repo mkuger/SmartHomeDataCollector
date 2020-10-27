e57262e0a91f52e76cb043e8ad7f361b0ad2daa4
mkuger <michael.kuger@gmail.com>
2020-10-27 20:43:49 +0100

    WIP

--- src/main/kotlin/bsh/DeviceRegistry.kt
+++ src/main/kotlin/bsh/DeviceRegistry.kt
@@ -1,10 +1,21 @@
 package bsh
 
+import java.time.Instant
+
 object DeviceRegistry {
 
     var devices: Array<Device> = emptyArray()
+        set(value) {
+            lastUpdated = Instant.now()
+            field = value
+        }
 
     fun deviceById(id: String) {
         devices.filter { d -> d.id == id }
     }
+
+    var lastUpdated: Instant = Instant.MIN
+        private set
+
+
 }
