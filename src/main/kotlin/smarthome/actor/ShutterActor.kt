59dd7cec269106f633ff887baf425b732be5c64d
Michael Kuger <michael.kuger@gmail.com>
2020-11-09 16:59:54 +0100

    handling exception

--- src/main/kotlin/smarthome/actor/ShutterActor.kt
+++ src/main/kotlin/smarthome/actor/ShutterActor.kt
@@ -18,29 +18,33 @@ fun CoroutineScope.shutterActor(device: Device) = actor<Service> {
     var timestamp = Instant.MIN
 
     for (msg in channel) { // iterate over incoming messages
-        if (msg.state !is ShutterContactState)
-            continue
-        if (msg.deviceId != device.id)
-            continue
-        log.info("Message received: $msg")
-        when (val shutterState = (msg.state as ShutterContactState).value) {
-            "OPEN" -> {
-                currentState = shutterState
-                timestamp = Instant.now()
-            }
-            "CLOSED" -> {
-                if (currentState != "OPEN") {
-                    log.info("Skipping. Was not open before.")
-                    continue
+        try {
+            if (msg.state !is ShutterContactState)
+                continue
+            if (msg.deviceId != device.id)
+                continue
+            log.info("Message received: $msg")
+            when (val shutterState = (msg.state as ShutterContactState).value) {
+                "OPEN" -> {
+                    currentState = shutterState
+                    timestamp = Instant.now()
                 }
-                val now = Instant.now()
-                val tags = listOf(RoomRegistry.byId(device.roomId).name, device.name, "smart-home", "ventilation")
-                val annotation = Annotation(timestamp.toEpochMilli(), now.toEpochMilli(), tags, "ventilation")
-                GrafanaClient.addAnnotation(annotation)
-                log.debug("Sending event to Grafana")
-                currentState = shutterState
-                timestamp = now
+                "CLOSED" -> {
+                    if (currentState != "OPEN") {
+                        log.info("Skipping. Was not open before.")
+                        continue
+                    }
+                    val now = Instant.now()
+                    val tags = listOf(RoomRegistry.byId(device.roomId).name, device.name, "smart-home", "ventilation")
+                    val annotation = Annotation(timestamp.toEpochMilli(), now.toEpochMilli(), tags, "ventilation")
+                    log.debug("Sending event to Grafana")
+                    GrafanaClient.addAnnotation(annotation)
+                    currentState = shutterState
+                    timestamp = now
+                }
             }
+        } catch (e: RuntimeException) {
+            log.warn("Exception processing message", e)
         }
     }
 }
